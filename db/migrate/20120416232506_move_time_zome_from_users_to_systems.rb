5e187c3ff865d81ad687cb713274e9208f45c90c
Adam Zaninovich <adam.zaninovich@gmail.com>
2012-04-16 23:53:00 -0700

    fixes migration error

--- db/migrate/20120416232506_move_time_zome_from_users_to_systems.rb
+++ db/migrate/20120416232506_move_time_zome_from_users_to_systems.rb
@@ -1,6 +1,6 @@
 class MoveTimeZomeFromUsersToSystems < ActiveRecord::Migration
   def change
-    remove_column :users, :time_zone, :string
+    remove_column :users, :time_zone
     add_column    :systems, :time_zone, :string, default: "Pacific Time (US & Canada)"
   end
 end
